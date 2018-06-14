433b5a90db9dfa6d7ce4f903ef4f89f92fb3d7d3
Daniel Flórez <danielgiovanny2@gmail.com>
2018-06-13 19:28:42 -0500

    inicio proyecto clase ManejoDeVariables.java

---
+++ EjerciciosJavaUdemy/src/ManejoDeVariables.java
@@ -0,0 +1,12 @@
+class ManejoDeVariables{
+    public static void main(String[] args) {
+   
+        int a=0; 
+        int b=20; 
+        double c=30.3; 
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+}
+}
