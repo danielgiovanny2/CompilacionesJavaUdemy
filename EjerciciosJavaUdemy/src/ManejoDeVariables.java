4ccd23c448de7cdd60f83030150da8b6a3852f19
Daniel Flórez <danielgiovanny2@gmail.com>
2018-06-13 19:36:13 -0500

    realizamos una modificación de prueba aumentando una variable double d.

--- EjerciciosJavaUdemy/src/ManejoDeVariables.java
+++ EjerciciosJavaUdemy/src/ManejoDeVariables.java
@@ -5,8 +5,12 @@ class ManejoDeVariables{
         int b=20; 
         double c=30.3; 
         
+        
+        //realizamos alguna modificación en el archivo para que se suba el branch
+        double d=40.3; 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+        System.out.println(d);
 }
 }
